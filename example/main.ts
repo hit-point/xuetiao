f51dff04d0fd814b03e467c23e00fcbfc65cd739
pasco152 <3110175163@qq.com>
2024-09-24 14:18:53 +0800

    feat: add components

--- example/main.ts
+++ example/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from "vue";
 import App from "./app.vue";
+import xuetiao from "@xuetiao/components";
 const app = createApp(App);
+app.use(xuetiao);
 app.mount("#app");
---
+++ packages/components/src/button/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="hp-button" :class="buttonStyle"><slot /></button>
+</template>
+
+<script lang="ts" setup>
+import "./style/index.less";
+import { computed } from "vue";
+defineOptions({ name: "hp-button" });
+type ButtonProps = {
+  type?: string;
+};
+const buttonProps = defineProps<ButtonProps>();
+
+const buttonStyle = computed(() => {
+  return { [`hp-button--${buttonProps.type}`]: buttonProps.type };
+});
+</script>
+
+

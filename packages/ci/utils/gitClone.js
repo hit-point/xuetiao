0b4a68561cabf0a7755134b0b37de1f7f887ef69
pasco152 <3110175163@qq.com>
2024-09-25 14:25:17 +0800

    add ci

---
+++ packages/ci/utils/gitClone.js
@@ -0,0 +1,24 @@
+import download from 'download-git-repo';
+import chalk from 'chalk';
+import ora from 'ora';
+
+export default (remote, name, option) => {
+  const downSpinner = ora('正在下载模板...').start();
+  return new Promise((resolve, reject) => {
+    download(remote, name, option, (err) => {
+      if (err) {
+        downSpinner.fail();
+        console.log('err', chalk.red(err));
+        reject(err);
+        return;
+      }
+      downSpinner.succeed(chalk.green('模板下载成功！'));
+      // 仅作展示
+      console.log(chalk.green(`cd ${name}\r\n`));
+      console.log(chalk.blue('pnpm bootstrap\r\n'));
+      console.log('pnpm run build:ui\r\n');
+      console.log('pnpm run dev:example\r\n');
+      resolve();
+    });
+  });
+};
